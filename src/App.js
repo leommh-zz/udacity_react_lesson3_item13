973555660eb007725816ac3a99e9335d5f8e0744
Leonardo Morini <leo.morini.nn@gmail.com>
2018-10-10 01:44:46 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+ 
+    state = {
+    	inputValue: 'valor'
+    }
+    
+    editText = (event) => {
+        const text = event.target.value
+      
+    	this.setState({
+        	inputValue: text
+        })
+     
+    } 
+  
+  render() {
+   
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">ReactND - Coding Practice</h1>
+        </header>
+        <div className="container">
+          <input type="text" placeholder="Say Something" onChange={ this.editText } />
+          <p className="echo">Echo:</p>
+          <p> { this.state.inputValue } </p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
